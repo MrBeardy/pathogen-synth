1c61883a981525fb6c54f4a10eb1fdae39c0f63b
Michael Hibbs <michael.hibbs@onthebeach.co.uk>
2017-01-13 16:18:18 +0000

    :cake: Initial commit
    This inital commit has a basic Knob component and a basic App
    interface for playing a random note.

---
+++ app/Utils.jsx
@@ -0,0 +1,24 @@
+export class ArcUtils {
+  static polarToCartesian(centerX, centerY, radius, angleInDegrees) {
+    var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
+
+    return {
+      x: centerX + (radius * Math.cos(angleInRadians)),
+      y: centerY + (radius * Math.sin(angleInRadians))
+    };
+  }
+
+  static describeArc(x, y, radius, startAngle, endAngle){
+    var start = this.polarToCartesian(x, y, radius, endAngle);
+    var end = this.polarToCartesian(x, y, radius, startAngle);
+
+    var largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
+
+    var d = [
+        "M", start.x, start.y,
+        "A", radius, radius, 0, largeArcFlag, 0, end.x, end.y
+    ].join(" ");
+
+    return d;
+  }
+}
